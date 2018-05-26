1fc8a8cf4dfdf421d617c3c4135a479a7baa1738
LittleElijah <cuiyeqing960904@gmail.com>
2018-05-27 00:32:02 +0300

    global .env function setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,3 +5,8 @@ use App\Config\Config;
 session_start();
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
+
+var_dump(getenv('APP'));
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,6 +7,10 @@ namespace Composer\Autoload;
 class ComposerStaticInitca1508cf6129ca0bb700bec6c4266e82
 {
     public static $prefixLengthsPsr4 = array (
+        'D' => 
+        array (
+            'Dotenv\\' => 7,
+        ),
         'A' => 
         array (
             'App\\' => 4,
@@ -14,6 +18,10 @@ class ComposerStaticInitca1508cf6129ca0bb700bec6c4266e82
     );
 
     public static $prefixDirsPsr4 = array (
+        'Dotenv\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/vlucas/phpdotenv/src',
+        ),
         'App\\' => 
         array (
             0 => __DIR__ . '/../..' . '/app',
