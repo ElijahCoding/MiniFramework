18ca2300ac760e9e1b2a5bc97235a1e1ffc91d50
LittleElijah <cuiyeqing960904@gmail.com>
2018-05-28 12:47:53 +0300

    done

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,7 +1,13 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-
 $container = new League\Container\Container;
 
-$container->addServiceProvider(new AppServiceProvider());
+$container->delegate(
+    new League\Container\ReflectionContainer
+);
+
+$container->addServiceProvider(new App\Providers\ConfigServiceProvider());
+
+foreach ($container->get('config')->get('app.providers') as $provider) {
+    $container->addServiceProvider($provider);
+}
